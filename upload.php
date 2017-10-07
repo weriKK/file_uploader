f1ca50b603e90d775e4f2b25ab37c4242b74d6fa
Kova <bitbucket@werik.com>
2017-10-07 18:01:54 +0200

    Randomized filenames, better responses

--- upload.php
+++ upload.php
@@ -2,6 +2,15 @@
 
 $allowed = array('png', 'jpg', 'gif', 'bmp', 'zip', '7z');
 
+function echoResponse($status, $filename, $url){
+    if($status === "success"){
+        return '{"files": [ { "name": "'.$filename.'", "url": "'.$url.'" } ] }';
+    }
+    else {
+        return '{"files": [ { "name": "'.$filename.'", "error": "error" } ] }';
+    }
+}
+
 $files = NULL;
 if(isset($_FILES['upl'])){
     $files = $_FILES['upl'];
@@ -10,16 +19,19 @@ if(isset($_FILES['upl'])){
 if((NULL != $files) && $files['error'] == 0){
     $ext = pathinfo($files['name'], PATHINFO_EXTENSION);
     if(!in_array(strtolower($ext), $allowed)){
-        echo '{"status":"error"}';
+        echoResponse("error", $files['name'], NULL);
         exit;
     }
 
-    if(move_uploaded_file($files['tmp_name'], '/home/uploads/'.$files['name'])){
-        echo '{"status":"success"}';
+    $filename = tempnam('/home/uploads', '');
+    unlink($filename);
+    if(move_uploaded_file($files['tmp_name'], $filename)){
+        $url = "http://werik.com/dl?".basename($filename);
+        echoResponse("success", $files['name'], $url);
         exit;
     }
 }
 
-echo '{"status":"error"}';
+echoResponse("error", NULL, NULL);
 exit;
 
